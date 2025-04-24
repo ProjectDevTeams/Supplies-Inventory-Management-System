011f1c2e62a74931b5dd8cc0743201ef1fc41192
pakorn <kb.h0liday44@gmail.com>
2025-04-24 15:27:06 +0700

    เพิ่ม popup รายการ

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./pages/login";
 import Consumable from "./pages/consumable";
-// import Consumable from "./components/Consumablebar";
+import AddnewPopup from "./components/addnew-popup";
+
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <Route index element={<Login/>} />
           <Route index path='/login' element={<Login/>}/>
           <Route index path='/consumable' element={<Consumable/>}/>
-          {/* <Route index path='/consumablebar' element={<Consumable/>}/> */}
+          <Route index path='/popup' element={<AddnewPopup/>}/>
         </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/addnew-popup.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "./addnew-popup.css";
+
+function AddnewPopup() {
+  return (
+    <div className="popup-container">
+      <div className="popup-box">
+        <div className="popup-header">
+          <span>เพิ่มรายการใหม่</span>
+          <button className="close-btn">✕</button>
+        </div>
+
+        <div className="popup-body">
+          <form>
+            <div className="form-row">
+              <label>ชื่อ</label>
+              <input type="text" />
+            </div>
+
+            <div className="form-row">
+              <label>ประเภท</label>
+              <select>
+                <option value="">ประเภท</option>
+                <option value="office">วัสดุสำนักงาน</option>
+                <option value="safety">วัสดุความปลอดภัย</option>
+              </select>
+            </div>
+
+            <div className="form-row">
+              <label>หน่วยนับ</label>
+              <input list="units" name="unit" />
+              <datalist id="units">
+                <option value="กระป๋อง" />
+                <option value="กระปุก" />
+                <option value="ก้อน" />
+                <option value="กิโลกรัม" />
+                <option value="แกลลอน" />
+                <option value="ขวด" />
+                <option value="คู่" />
+                <option value="เครื่อง" />
+              </datalist>
+            </div>
+
+            <div className="form-row">
+              <label>คลังวัสดุ</label>
+              <select>
+                <option value="">เลือกคลังวัสดุ</option>
+                <option value="inside">วัสดุในคลัง</option>
+                <option value="outside">วัสดุนอกคลัง</option>
+              </select>
+            </div>
+
+            <div className="form-row">
+              <label>ยอดยกมา</label>
+              <input type="number" />
+            </div>
+
+            <div className="form-row">
+              <label>ยอดต่ำสุด</label>
+              <input type="number" />
+            </div>
+
+            <div className="form-row">
+              <label>ยอดสูงสุด</label>
+              <input type="number" />
+            </div>
+
+            <div className="form-row">
+              <label>ราคา/หน่วย</label>
+              <input type="number" />
+            </div>
+
+            <div className="form-row file-upload">
+              <label>แนบไฟล์ภาพ</label>
+              <div className="upload-group">
+                <small>ขนาดไฟล์สูงสุด 5MB</small>
+    
+                <label htmlFor="fileUpload" className="custom-file-btn">เลือกไฟล์</label>
+                <input type="file" id="fileUpload" className="file-hidden" />
+              </div>
+              </div>
+
+            <div className="form-row">
+              <label>วันที่สร้าง</label>
+              <input type="date" />
+            </div>
+
+            <div className="form-footer">
+              <button type="submit" className="submit-btn">ยืนยัน</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddnewPopup;
+
