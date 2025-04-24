0c9d3a808a2cc644bfe3a4d488e962b483d82475
Tawicha Sirisuwan <tawicha9643@outlook.com>
2025-04-24 11:22:21 +0700

    เพิ่ม routing Comsum

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./pages/login";
+import Consumable from "./components/Consumablebar";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route index element={<Login/>} />
           <Route index path='/login' element={<Login/>}/>
+          <Route index path='/consumablebar' element={<Consumable/>}/>
         </Routes>
     </BrowserRouter>
   );
