15c340a06dade6bad3ca5bfb46f974b0de9ebb4c
Tawicha Sirisuwan <tawicha9643@outlook.com>
2025-04-24 08:59:25 +0700

    Login Page

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './Main.css';
 import Navbar from "../components/Navbar";
-
+import Login from "../pages/login"
 function Main() {
     return (
     <body>
       <title>Supply System</title>
       
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Login/>
     </body>
       
     );
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,37 +1,38 @@
+import React from 'react';
 import './login.css';
 
 function Login() {
   return (
-    <div className="login-container">
-      <div className="login-left">
-        <div className="login-box">
-          <img src="/image/logo.png" alt="Logo" className="logo" />
-          <h4>Welcome back!</h4>
-          <h2>กรุณาเข้าสู่ระบบ</h2>
+    <body className="body-login">
+      <div className="login-container">
+        <div className="login-left">
+          <div className="login-box">
+            <img src="/image/logo.png" alt="Logo" className="logo" />
+            <h4>Welcome back!</h4>
+            <h2>กรุณาเข้าสู่ระบบ</h2>
 
-          <div className="input-group">
-            <label>Username</label>
-            <input type="text" placeholder="Enter name" />
+            <div className="input-group">
+              <label>Username</label>
+              <input type="text" placeholder="Enter name"  />
+            </div>
+
+            <div className="input-group">
+              <label>Password</label>
+              <input type="password" placeholder="Enter password"  />
+            </div>
+
+            <div className="forgot-password">ลืมรหัสผ่าน</div>
+
+            <button className="login-button">เข้าสู่ระบบ</button>
           </div>
+        </div>
 
-          <div className="input-group">
-            <label>Password</label>
-            <input type="password" placeholder="Enter password" />
-          </div>
-
-          <div className="forgot-password">ลืมรหัสผ่าน</div>
-
-          <button className="login-button">เข้าสู่ระบบ</button>
+        <div className="login-right">
+          {/* รูปพื้นหลังแสดงจาก CSS */}
         </div>
       </div>
-
-      <div className="login-right">
-        {/* ฝั่งขวาใส่รูปเป็นพื้นหลัง */}
-      </div>
-    </div>
+    </body>
   );
 }
 
 export default Login;
-
-/**/
