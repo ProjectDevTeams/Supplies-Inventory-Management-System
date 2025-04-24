e2244ef53ec530d15e84c6c09415e43557bd8c80
Tawicha Sirisuwan <tawicha9643@outlook.com>
2025-04-24 15:15:04 +0700

    รวมComponent

--- client/src/pages/consumable.js
+++ client/src/pages/consumable.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-
+import Consumablebar from '../components/Consumablebar';
 function ConsumablePage() {
   return (
     <div>
       <Navbar />
       <Sidebar />
+      <div id='Consumabslebar'>
+        <Consumablebar />
+      </div>
+     
+     
     </div>
 
   );
