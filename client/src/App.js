1efa695a047d827277f5245e9108ee941f4c5b28
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2025-04-25 11:19:02 +0700

    เพิ่มหน้า HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ConsumablePage from "./pages/ConsumablePage";
@@ -15,7 +16,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LoginPage />} />
+        <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/consumable" element={<ConsumablePage />} />
