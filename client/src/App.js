13e08afd832761f437ae4e3234a658a8c2558b22
Tawicha Sirisuwan <tawicha9643@outlook.com>
2025-04-25 11:49:43 +0700

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 
 
-// import Consumable from "./components/Consumablebar";
-
-=======
 import HomePage from "./pages/HomePage";
->>>>>>> 2c3dabe2a86ac7e23064f1dfaa2ee7c17057de61
+
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ConsumablePage from "./pages/ConsumablePage";
 import StuffPage from "./pages/StuffPage";
-<<<<<<< HEAD
-
-=======
 import HistoryPage from "./pages/HistoryPage";
 import HumanPage from "./pages/HumanPage";
 import OrganizationsPage from "./pages/OrganizationsPage";
 import ReportPage from "./pages/ReportPage"
 import PermissionPage from "./pages/PermissionPage"
 import SettingPage from "./pages/SettingPage";
->>>>>>> 2c3dabe2a86ac7e23064f1dfaa2ee7c17057de61
+
 
 function App() {
   return (
