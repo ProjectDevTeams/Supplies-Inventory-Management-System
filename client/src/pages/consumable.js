12289e43f228d82478c66ce34c529b74277e6b9d
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2025-04-25 09:00:37 +0700

    แก้ไขหน้า ConsumablePage

--- client/src/pages/consumable.js
+++ client/src/pages/consumable.js
@@ -2,19 +2,21 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Consumablebar from '../components/Consumablebar';
+import './comsumable.css';
+
 function ConsumablePage() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Sidebar />
-      <div id='Consumabslebar'>
-        <Consumablebar />
+      <div className="page-body">
+        <Sidebar />
+        <div className="main-area">
+          <Consumablebar />
+        </div>
       </div>
-     
-     
-    </div>
-
+    </>
   );
 }
+
 
 export default ConsumablePage;
