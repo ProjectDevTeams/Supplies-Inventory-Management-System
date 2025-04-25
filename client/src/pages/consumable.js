61e1e8f2c4e5562bb24871b73023453823f4c215
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2025-04-25 09:09:33 +0700

    แก้ไข css ในหน้า consumable

--- client/src/pages/consumable.js
+++ client/src/pages/consumable.js
@@ -2,21 +2,22 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Consumablebar from '../components/Consumablebar';
-import './comsumable.css';
+import './consumable.css';
 
 function ConsumablePage() {
   return (
-    <>
+    <div className="consumable-navbar">
       <Navbar />
-      <div className="page-body">
+      <div className="consumable-sidebar">
         <Sidebar />
-        <div className="main-area">
-          <Consumablebar />
-        </div>
+        <main className="consumable-content">
+          <section className="content-header">
+            <Consumablebar />
+          </section>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
-
 
 export default ConsumablePage;
