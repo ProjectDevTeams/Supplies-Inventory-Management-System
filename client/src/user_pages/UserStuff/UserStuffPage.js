5eb2f266b4ba64678cfb698417a2e1572c49aaef
kittinartgithub <kittinart_wave9283@hotmail.com>
2025-05-01 11:47:30 +0700

    ค2

--- client/src/user_pages/UserStuff/UserStuffPage.js
+++ client/src/user_pages/UserStuff/UserStuffPage.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
-<<<<<<< HEAD
-
 import UserStuffbar from '../../user_components/UserStuff/UserStuff_bar';
 
 
-=======
-import UserStuffbar from '../../user_components/UserStuff/UserStuff_bar';
-// import UserStuff_Table from '../../user_components/UserStuff/UserStuff_table';
->>>>>>> fc8f7c7568a0d35cef1b24980eda1f58b3e336f9
+
+
+
 import './UserStuffPage.css';
 
 function UserStuffPage() {
@@ -18,16 +15,11 @@ function UserStuffPage() {
     <div className="userstuff-navbar">
       <Navbar />
       <main className="userstuff-content">  
-<<<<<<< HEAD
-
 
 
           <UserStuffbar />
 
-=======
-          <UserStuffbar />
-          {/* <UserStuff_Table/> */}
->>>>>>> fc8f7c7568a0d35cef1b24980eda1f58b3e336f9
+
       </main>
     </div>
   );
