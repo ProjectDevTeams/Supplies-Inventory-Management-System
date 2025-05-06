d7f4b4a295bc4d9e4b63c014a8a8522041d1f8bd
kittinartgithub <kittinart_wave9283@hotmail.com>
2025-05-06 11:21:04 +0700

    เพิ่มหน้า /Adjust-add-popup

--- client/src/components/Adjust/Adjustbar.js
+++ client/src/components/Adjust/Adjustbar.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import "./Adjustbar.css";
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AdjustAddPopup from "./Adjust-add-popup";
 
-
-function Adjustbar( { onAddClick, searchTerm, setSearchTerm } ) {
-
+function Adjustbar({ onAddClick, searchTerm, setSearchTerm }) {
   const navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
+
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
@@ -19,18 +20,24 @@ function Adjustbar( { onAddClick, searchTerm, setSearchTerm } ) {
       <div className="adjust-controls">
         {/* ช่องค้นหา */}
         <div className="search-container">
-        <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
           <input
             type="text"
             placeholder="ค้นหา"
             value={searchTerm}
             onChange={handleSearchChange}
           />
-        
         </div>
 
-        {/* ปุ่มเพิ่มหัวข้อ */}
-        <button className="btn green">+ เพิ่มหัวข้อ</button>
+        <div>
+          <button className="btn green" onClick={() => setShowPopup(true)}>
+            + เพิ่มหัวข้อ
+          </button>
+
+          {showPopup && (
+            <AdjustAddPopup onClose={() => setShowPopup(false)} />
+          )}
+        </div>
       </div>
     </div>
   );
